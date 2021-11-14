584c514dffeab3e678886481155bcff2278e5408
wold21 <wold2180@gmail.com>
2021-11-14 16:15:04 +0900

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button v-on:click="onClick()" :style="{background : color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name : 'Button',
+    props : {
+        text : String,
+        color : String,
+    },
+    methods : {
+        onClick() {
+             console.log("click")
+        }
+    }
+
+}
+</script>
