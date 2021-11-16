37e3929aec2cdb1b121746b6b09575c58eec9643
hyuk.kim <hyuk.kim@konantech.com>
2021-11-17 08:55:20 +0900

    toggle-button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="container">
-    <Header title="Task Tracker"/>
-    <AddTask @add-task="addTask"/>
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask"/>
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask"/>
+    </div>
     <Tasks @toggle-reminder="reminderSwitch" @delete-task="deleteTask" :tasks="tasks"/>
 </div>
 </template>
@@ -20,10 +22,14 @@ export default {
   },
   data(){
     return {
-      tasks: []
+      tasks: [],
+      showAddTask : false
     }
   },
   methods : {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
     addTask(task){
       console.log("main", task)
       this.tasks = [...this.tasks, task]
